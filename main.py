cb4ab869bf8510e8655cf2997b3242ed74bc9a5f
KhachDavid <khachatryandavid1@gmail.com>
2024-09-17 14:31:10 -0500

    Fix line segments and citations

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 #
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
 
 from modules.Graph import Graph
 from modules.Node import Node
@@ -27,7 +28,7 @@ def main():
     
     xpoints, ypoints = graph.get_points()
     setup_mpl()
-    run_mpl(xpoints, ypoints)
+    run_mpl(xpoints, ypoints, graph.edges)
 
 def setup_mpl():
     ########## Begin_Citation [1] ##########
@@ -36,9 +37,38 @@ def setup_mpl():
     ax.set_ylim([0, 100])
     ########## End_Citation [1] ##########
 
-def run_mpl(xpoints, ypoints):
-    plt.plot(xpoints, ypoints, 'o', markersize=1)
+def run_mpl(xpoints, ypoints, edges):
+    ########## Begin_Citation [3] ##########
+
+    # Create a figure and an axis
+    fig, ax = plt.subplots()
+
+    # Convert Edge objects into a list of line segments
+    formatted_edges = [
+        (edge.get_point_one(), edge.get_point_two()) for edge in edges
+    ]
+    
+    # Extract all vertices from the edges (flattened list of points)
+    points = [point for edge in formatted_edges for point in edge]
+    
+    # Separate x and y coordinates of all points
+    xpoints, ypoints = zip(*points)
+    
+    # Plot the vertices as points
+    ax.plot(xpoints, ypoints, 'o', markersize=2)
+    
+    # Create a LineCollection from the edges
+    line_segments = LineCollection(formatted_edges, linewidths=1)
+    
+    # Add the line collection to the axis
+    ax.add_collection(line_segments)
+    
+    # Set limits for the plot
+    ax.autoscale()
     plt.savefig('linear.pdf')
+
+    ########## End_Citation [3] ##########
+
 
 if __name__ == "__main__":
     main()
